e3f7120721644c19ca2ffbd395f0acadce770ed2
Adam Smelko <adamsmelko1@gmail.com>
2023-10-04 20:45:36 +0200

    fix kick the tires

--- benchmark/preprocess.py
+++ benchmark/preprocess.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python3
 
 import os
-import sys
+import shutil
 
 from shared import Benchmark
 
 from pathlib import Path
-from typing import List, Tuple, Dict
 
 benchmark = Benchmark.load(Path.cwd() / "out")
 
-if not os.path.exists("results"):
-    os.mkdir("results")
+if os.path.exists("results"):
+    shutil.rmtree("results")
+os.mkdir("results")
 
 for group_name, group in benchmark.groups.items():
     os.mkdir(f"results/{group_name}")
--- kick-the-tires.sh
+++ kick-the-tires.sh
@@ -2,6 +2,7 @@
 ./benchmark.sh one-to-one/build/cross one_to_one_fast
 
 cd plots
+rm -rf one-to-one-fast
 mkdir one-to-one-fast
 Rscript plot_fast.R
 cd -
